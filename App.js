272506e5ce8cf76e408e64920f2603cb63799dc2
Ximena <ximena.garcia7482@alumnos.udg.mx>
2022-04-11 23:06:01 -0500

    headertab y searchbar

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import { View, Text } from "react-native";
+import Home from "./screens/Home";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+  return <Home />;
 }
